a2c4cd2264ff43eb8adcb98a752430a6eab734f8
Jason Axelson <jason.axelson@gmail.com>
2023-02-12 11:18:26 -1000

    update assets to explicitly include scenic assets

--- lib/assets.ex
+++ lib/assets.ex
@@ -1,4 +1,10 @@
 defmodule Dash.Assets do
   use Scenic.Assets.Static,
-    otp_app: :dash
+    otp_app: :dash,
+    sources: [
+      "assets",
+      {:scenic, "deps/scenic/assets"}
+    ]
+
+  def asset_path, do: Path.join([__DIR__, "..", "..", "assets"]) |> Path.expand()
 end
