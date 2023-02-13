8d2faabe24be800710093776b0fb06c29265e8e0
Jason Axelson <jason.axelson@gmail.com>
2023-02-12 19:35:02 -1000

    Don't build a graph at compile-time

--- lib/scenes/color_test.ex
+++ lib/scenes/color_test.ex
@@ -5,29 +5,31 @@ defmodule Dash.Scene.ColorTest do
   import Scenic.Primitives
   require Logger
 
-  @graph Graph.build(fill: :black, font: :roboto)
-         |> rectangle({600, 64}, t: {0, 64}, fill: :white)
-         |> rectangle({600, 64}, t: {0, 128}, fill: :green)
-         |> rectangle({600, 64}, t: {0, 192}, fill: :blue)
-         |> rectangle({600, 64}, t: {0, 256}, fill: :red)
-         |> rectangle({600, 64}, t: {0, 320}, fill: :yellow)
-         |> rectangle({600, 64}, t: {0, 384}, fill: :orange)
-         |> text("Black", font_size: 36, t: {24, 38}, fill: :white)
-         |> text("White", font_size: 36, t: {24, 102}, fill: :black)
-         |> text("Green", font_size: 36, t: {24, 166}, fill: :white)
-         |> text("Blue", font_size: 36, t: {24, 230}, fill: :white)
-         |> text("Red", font_size: 36, t: {24, 294}, fill: :white)
-         |> text("Yellow", font_size: 36, t: {24, 358}, fill: :black)
-         |> text("Orange", font_size: 36, t: {24, 422}, fill: :white)
+  defp initial_graph do
+    Graph.build(fill: :black, font: :roboto)
+    |> rectangle({600, 64}, t: {0, 64}, fill: :white)
+    |> rectangle({600, 64}, t: {0, 128}, fill: :green)
+    |> rectangle({600, 64}, t: {0, 192}, fill: :blue)
+    |> rectangle({600, 64}, t: {0, 256}, fill: :red)
+    |> rectangle({600, 64}, t: {0, 320}, fill: :yellow)
+    |> rectangle({600, 64}, t: {0, 384}, fill: :orange)
+    |> text("Black", font_size: 36, t: {24, 38}, fill: :white)
+    |> text("White", font_size: 36, t: {24, 102}, fill: :black)
+    |> text("Green", font_size: 36, t: {24, 166}, fill: :white)
+    |> text("Blue", font_size: 36, t: {24, 230}, fill: :white)
+    |> text("Red", font_size: 36, t: {24, 294}, fill: :white)
+    |> text("Yellow", font_size: 36, t: {24, 358}, fill: :black)
+    |> text("Orange", font_size: 36, t: {24, 422}, fill: :white)
+  end
 
   @impl Scenic.Scene
   def init(scene, _params, _opts) do
-    state = %{graph: @graph}
+    state = %{graph: initial_graph()}
     Logger.info("Started :mainscene")
 
     Process.register(self(), :mainscene)
     scene = assign(scene, :state, state)
-    scene = push_graph(scene, @graph)
+    scene = push_graph(scene, state.graph)
     {:ok, scene}
   end
 
