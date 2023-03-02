c90d12e407f785868cdbe3e399643178ef4e1adc
Jason Axelson <jason.axelson@gmail.com>
2023-03-01 19:43:14 -1000

    start tests for color parsing

--- test/web_color_parser_test.exs
+++ test/web_color_parser_test.exs
@@ -4,4 +4,15 @@ defmodule Dash.WebColorParserTest do
   alias Dash.WebColorParser
 
   doctest(WebColorParser)
+
+  test "named colors" do
+    WebColorParser.parse("red")
+    |> IO.inspect(label: "res (web_color_parser_test.exs:10)")
+  end
+
+  test "hexcode" do
+    # WebColorParser.parse("#FF0000")
+    WebColorParser.parse("rgb(255, 0, 0)")
+    |> IO.inspect(label: "res (web_color_parser_test.exs:15)")
+  end
 end
