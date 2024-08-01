5118b435e80a5ef129620183169715e8e34f2c54
Jason Axelson <jason.axelson@gmail.com>
2024-08-01 07:30:04 -0500

    We don't support parsing name colors right now

--- test/web_color_parser_test.exs
+++ test/web_color_parser_test.exs
@@ -5,11 +5,6 @@ defmodule Dash.WebColorParserTest do
 
   doctest(WebColorParser)
 
-  test "named colors" do
-    WebColorParser.parse("red")
-    |> IO.inspect(label: "res (web_color_parser_test.exs:10)")
-  end
-
   test "hexcode" do
     # WebColorParser.parse("#FF0000")
     WebColorParser.parse("rgb(255, 0, 0)")
