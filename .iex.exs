6f08c20c1fba3e649243a39221377ac0384a7adf
Jason Axelson <jason.axelson@gmail.com>
2024-08-03 08:38:01 -0500

    update iex script

--- .iex.exs
+++ .iex.exs
@@ -1,3 +1,7 @@
 if Code.ensure_loaded?(ExSync) && function_exported?(ExSync, :register_group_leader, 0) do
   ExSync.register_group_leader()
 end
+
+global_iex_path = Path.expand("~/.iex.exs")
+if File.exists?(global_iex_path), do: Code.eval_file("~/.iex.exs")
+import_if_available(JaxIEx)
